ba726f14f949b3a31e1e0ea11794e1df99719d01
Rafael Castro <rccastrorafael@gmail.com>
2024-02-29 06:42:32 -0300

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -8,7 +8,6 @@ export async function getGeoLocation({ limit = 10, search }: { limit?: number; s
 }
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
-  // https://api.openweathermap.org/data/3.0/onecall?lat=-28.48&lon=-49.00&appid=25ef542799c468209677e5c1b1c07564
   const request = await fetch(
     `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}${apikey}`,
   );
